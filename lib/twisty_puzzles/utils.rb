bf66d088cc80b5395493ee9538590578505e2a91
Bernhard Brodowsky <bernhard.brodowsky@gmail.com>
2020-04-21 23:35:25 +0200

    Made it easier to require twisty puzzles stuff

--- lib/twisty_puzzles/utils.rb
+++ lib/twisty_puzzles/utils.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'twisty_puzzles/utils/array_helper'
+require 'twisty_puzzles/utils/string_helper'
+
 module CubeTrainer
   # Module that contains various utils that aren't very specific to twisty puzzles.
   module Utils
