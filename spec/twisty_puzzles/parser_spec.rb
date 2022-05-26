461351d681af9bded011b897bb4e6164a3e4c945
Bernhard F. Brodowsky <bernhard.brodowsky@gmail.com>
2022-05-26 17:59:56 +0200

    Rubocop fixes

--- spec/twisty_puzzles/parser_spec.rb
+++ spec/twisty_puzzles/parser_spec.rb
@@ -3,53 +3,53 @@
 describe Parser do
   it 'parses a simple commutator' do
     expect(parse_commutator('[R, U]')).to eq(
-                                            PureCommutator.new(
-                                              Algorithm.move(FatMove.new(Face::R, CubeDirection::FORWARD)),
-                                              Algorithm.move(FatMove.new(Face::U, CubeDirection::FORWARD))
-                                            )
-                                          )
+      PureCommutator.new(
+        Algorithm.move(FatMove.new(Face::R, CubeDirection::FORWARD)),
+        Algorithm.move(FatMove.new(Face::U, CubeDirection::FORWARD))
+      )
+    )
   end
 
   it 'parses a simple commutator without brackets' do
     expect(parse_commutator('R, U')).to eq(
-                                          PureCommutator.new(
-                                            Algorithm.move(FatMove.new(Face::R, CubeDirection::FORWARD)),
-                                            Algorithm.move(FatMove.new(Face::U, CubeDirection::FORWARD))
-                                          )
-                                        )
+      PureCommutator.new(
+        Algorithm.move(FatMove.new(Face::R, CubeDirection::FORWARD)),
+        Algorithm.move(FatMove.new(Face::U, CubeDirection::FORWARD))
+      )
+    )
   end
 
   it 'parses two commutators with a plus' do
     expect(parse_commutator('[R, U] + [L, U]')).to eq(
-                                                     CommutatorSequence.new(
-                                                       [
-                                                         PureCommutator.new(
-                                                           Algorithm.move(FatMove.new(Face::R, CubeDirection::FORWARD)),
-                                                           Algorithm.move(FatMove.new(Face::U, CubeDirection::FORWARD))
-                                                         ),
-                                                         PureCommutator.new(
-                                                           Algorithm.move(FatMove.new(Face::L, CubeDirection::FORWARD)),
-                                                           Algorithm.move(FatMove.new(Face::U, CubeDirection::FORWARD))
-                                                         ),
-                                                       ]
-                                                     )
-                                                   )
+      CommutatorSequence.new(
+        [
+          PureCommutator.new(
+            Algorithm.move(FatMove.new(Face::R, CubeDirection::FORWARD)),
+            Algorithm.move(FatMove.new(Face::U, CubeDirection::FORWARD))
+          ),
+          PureCommutator.new(
+            Algorithm.move(FatMove.new(Face::L, CubeDirection::FORWARD)),
+            Algorithm.move(FatMove.new(Face::U, CubeDirection::FORWARD))
+          )
+        ]
+      )
+    )
   end
 
   it 'parses two commutators without brackets with a plus' do
     expect(parse_commutator('R, U + L, U')).to eq(
-                                                 CommutatorSequence.new(
-                                                   [
-                                                     PureCommutator.new(
-                                                       Algorithm.move(FatMove.new(Face::R, CubeDirection::FORWARD)),
-                                                       Algorithm.move(FatMove.new(Face::U, CubeDirection::FORWARD))
-                                                     ),
-                                                     PureCommutator.new(
-                                                       Algorithm.move(FatMove.new(Face::L, CubeDirection::FORWARD)),
-                                                       Algorithm.move(FatMove.new(Face::U, CubeDirection::FORWARD))
-                                                     ),
-                                                   ]
-                                                 )
-                                               )
+      CommutatorSequence.new(
+        [
+          PureCommutator.new(
+            Algorithm.move(FatMove.new(Face::R, CubeDirection::FORWARD)),
+            Algorithm.move(FatMove.new(Face::U, CubeDirection::FORWARD))
+          ),
+          PureCommutator.new(
+            Algorithm.move(FatMove.new(Face::L, CubeDirection::FORWARD)),
+            Algorithm.move(FatMove.new(Face::U, CubeDirection::FORWARD))
+          )
+        ]
+      )
+    )
   end
 end
